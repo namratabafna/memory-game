26bf361484b01439c7761956d75a3a791956dc60
Namrata Bafna <namrata.bafna@siemens.com>
2020-10-01 14:26:06 +0530

    Added code to work on hard  game level.

--- src/assets/index.js
+++ src/assets/index.js
@@ -14,52 +14,42 @@ import A12 from "./12.svg";
 import A13 from "./13.svg";
 import A14 from "./14.svg";
 import A15 from "./15.svg";
+import A16 from "./16.svg";
+import A17 from "./17.svg";
+import A18 from "./18.svg";
+import A19 from "./19.svg";
+import A20 from "./20.svg";
+import A21 from "./21.svg";
+import A22 from "./22.svg";
+import A23 from "./23.svg";
+import A24 from "./24.svg";
+import A25 from "./25.svg";
 
-export let images_svg = [
-  {
-    image:A1   
-  },
-  {
-    image:A2   
-  },
-  {
-    image:A3    
-  },
-  {
-    image:A4    
-  },
-  {
-    image:A5
-  },
-  {
-    image:A6
-  },
-  {
-    image:A7
-  },
-  {
-    image:A8
-  },
-  {
-    image:A9
-  },
-  {
-    image:A10
-  },
-  {
-    image:A11
-  },
-  {
-    image:A12
-  },
-  {
-    image:A13
-  },
-  {
-    image:A14
-  },
-  {
-    image:A15
-  }
+export let images_svg = [  
+ A1, 
+ A2,
+ A3,
+ A4,
+ A5,
+ A6,
+ A7,
+ A8,
+ A9,
+ A10,
+ A11,
+ A12,
+ A13,
+ A14,
+ A15,
+ A16,
+ A17,
+ A18,
+ A19,
+ A20,
+ A21,
+ A22,
+ A23,
+ A24,
+ A25
 ];
 
